d8c9ef37a39c0769c78b7fdbbc9af6a75dc4ceec
luigimicco <luigi.micco@gmail.com>
2022-01-25 16:29:05 +0100

    first step

--- js/script.js
+++ js/script.js
@@ -3,3 +3,90 @@ console.log("JS ok");
 // generatore di numeri random
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+const cars = [
+  {
+    name: "Luigi",
+    trackColor: "red",
+    id: "car-1",
+    currentPosition: 0,
+  },
+  {
+    name: "Matteo",
+    trackColor: "pink",
+    id: "car-2",
+    currentPosition: 0,
+  },
+];
+
+const setupScene = () => {
+  const scene = document.getElementById('scene');
+  scene.innerHTML = "";
+
+  cars.forEach( (car) => {
+    car.currentPosition = 0;  
+
+    // costruire la pista  newTrack
+    const newTrack = renderCar(car);
+
+    scene.append(newTrack);
+
+  });
+};
+
+const renderCar = (car) => {
+
+    const track = document.createElement("div");
+    track.style.background = car.trackColor;
+    track.classList.add('track');
+
+    const carBody = document.createElement("img");
+    carBody.id =car.id;
+    carBody.setAttribute("src", `./img/${car.id}.png`)
+    carBody.classList.add('car');
+    track.append(carBody);
+
+    const trackName = document.createElement("p");
+    trackName.innerText = car.name;
+    trackName.classList.add('name');
+    track.append(trackName);
+
+    return track;
+
+};
+
+const gameOver = (() => {
+
+
+  return false;
+});
+
+
+setupScene();
+
+document.getElementById('start').addEventListener('click', () => {
+
+  const playGame = setInterval( () => {
+    if (gameOver()) {
+      clearInterval(playGame);
+//      showResults();
+    } else {
+
+      cars.forEach( (car) => {
+        const carBody = document.getElementById(car.id);
+        car.currentPosition +=10;
+
+        if (car.currentPosition >= 800) {
+
+        } else {
+          carBody.style.left = car.currentPosition + "px";
+        }        
+
+      });
+
+    }
+
+  }, 200); 
+
+
+
+});
